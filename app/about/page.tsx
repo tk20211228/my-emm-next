18ebee998284dc86a47c66c0a80946222a71745c
tk20211228 <t3kuboki@gmail.com>
2024-10-04 16:38:39 +0900

    del: code delete

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,13 +5,6 @@ export default function Page() {
       About
       <ul>
         <li>リスト0</li>
-        <li>リスト1</li>
-        <li>リスト2</li>
-        <li>リスト3</li>
-        <li>リスト4</li>
-        <li>リスト5</li>
-        <li>リスト6</li>
-        <li>リスト7</li>
       </ul>
       <ul>
         <li></li>
